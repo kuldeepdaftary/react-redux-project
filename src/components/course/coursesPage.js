268d55d6841580d09c81f51496df7cdaa806f603
Kuldeep <Kuldeep.Daftary@gmail.com>
2017-05-01 20:30:45 +0100

    Course page react logic

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -1,10 +1,40 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: {
+        title: null
+      }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({
+      course
+    });
+  }
+
+  onClickSave () {
+    console.log(this);
+    alert(`Saving ${this.state.course.title}`);
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
+        <input type="submit" onClick={this.onClickSave} value="Save" />
       </div>
     );
   }
