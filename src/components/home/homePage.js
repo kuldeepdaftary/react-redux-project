0cf46bcd0052d1c44cd99da374eda665deadaa5a
Kuldeep <Kuldeep.Daftary@gmail.com>
2017-05-01 13:49:23 +0100

    Introduces Components

---
+++ src/components/home/homePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class HomePage extends React.Component {
+  render () {
+    return (
+      <div className="jumbotron">
+        <h1>React Redux Project</h1>
+        <p>A Simple react app to build upon, more of a testing ground</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
