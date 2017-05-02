fb8ae8cdc212b8fc47fe5a9cb3ec79b4171d5de9
Kuldeep <Kuldeep.Daftary@gmail.com>
2017-05-02 22:34:16 +0100

    actionType constants

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,7 @@
+import {CREATE_COURSE} from '../actions/actionTypes';
 export default function courses(state = [], action) {
   switch(action.type) {
-    case 'CREATE_COURSE':
+    case CREATE_COURSE:
       return [ ...state, Object.assign({}, action.course) ];
     default:
       return state;
