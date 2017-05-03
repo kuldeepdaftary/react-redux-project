9a9456f76c64dfded9ca9900c8933b34dbc407f2
Kuldeep <Kuldeep.Daftary@gmail.com>
2017-05-03 19:25:46 +0100

    Enters redux-thunk ... Pa pa pa

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,7 +1,19 @@
-import {CREATE_COURSE} from './actionTypes';
-export function createCourse(course) {
+import {LOAD_COURSES_SUCCESS} from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
+export function loadCoursesSuccess(course) {
   return {
-    type: CREATE_COURSE,
+    type: LOAD_COURSES_SUCCESS,
     course
+  }
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    })
   }
 }
--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -6,27 +6,6 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: {
-        title: null
-      }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(e) {
-    const course = this.state.course;
-    course.title = e.target.value;
-    this.setState({
-      course
-    });
-  }
-
-  onClickSave () {
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow (course, index) {
@@ -40,9 +19,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
-        <input type="submit" onClick={this.onClickSave} value="Save" />
       </div>
     );
   }
