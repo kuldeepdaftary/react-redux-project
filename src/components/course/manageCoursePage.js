6ad4630530cd09389616be76e61ef79acbbdf730
Kuldeep <Kuldeep.Daftary@gmail.com>
2017-05-03 20:22:43 +0100

    manageCoursePage

---
+++ src/components/course/manageCoursePage.js
@@ -0,0 +1,33 @@
+import React, {PropTypes} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
+
+class ManageCoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <h1>Manage Course</h1>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    state: state
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
